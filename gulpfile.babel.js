ad72216048ea10f6fa151cf9421f48b98ac926d3
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2020-09-16 16:54:47 +0900

    Setup: Gulp 2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,35 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
 
-// 경로 변수 정의
+sass.compiler = require("node-sass");
+
 const paths = {
   styles: {
     src: "assets/scss/styles.scss",
     dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss",
   },
 };
 
-export function styles() {
+function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles() {
+  gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
